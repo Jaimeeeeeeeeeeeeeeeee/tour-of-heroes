ee74a1d5f08fc14a33c198a3b64816d2b389f37a
Jaimeeeeeeeeeeeeeeeee <jaime.garcia@campuspolitecnicoaceimar.com>
2025-09-22 14:41:31 +0200

    añadimos componentes y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   providers: [
     provideClientHydration()

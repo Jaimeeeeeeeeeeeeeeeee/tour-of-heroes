731959d9dbe3a77eb58b1c115350889acd8a35b8
Jaimeeeeeeeeeeeeeeeee <jaime.garcia@campuspolitecnicoaceimar.com>
2025-09-23 14:55:29 +0200

    navegacion

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,7 @@ import {HEROES} from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
@@ -23,27 +24,21 @@ import { MessageService } from '../message.service';
     NgIf,
     NgFor,
     UpperCasePipe,
-    HeroDetailComponent
+    HeroDetailComponent,
+    RouterLink
   ],
 })
-
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { HeroDetailComponent } from './app/hero-detail/hero-detail.component';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  const routes: Routes = [
+    { path: 'hero/:id', component: HeroDetailComponent }
+  ];
+  
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideRouter(routes) // 👈 ahora sí existe
+    ]
+  });
